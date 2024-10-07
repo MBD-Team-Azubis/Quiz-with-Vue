ba14771a0ecb086c00f929cfa656e37426e894b5
PhilippKDT <p.kleidt@mbd-team.de>
2024-10-07 15:48:59 +0200

    added player name input and pointcount

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,45 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, reactive } from "vue";
 
-<template></template>
+let nameStore = ref("");
 
-<style scoped></style>
+const player = reactive({
+  name: "",
+  points: 0,
+});
+
+function storeName() {
+  player.name = nameStore.value;
+  console.log(player);
+}
+</script>
+
+<template>
+  <div class="screen">
+    <h1>OSI-Quiz</h1>
+    <input
+      type="text"
+      v-model="nameStore"
+      @keydown.enter="storeName()"
+      placeholder="Please enter your name:"
+      v-if="player.name === ''"
+    />
+    <div>
+      <h2>Your name: {{ player.name }}</h2>
+      <h2>Your points: {{ player.points }}</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.screen {
+  margin: 0;
+  height: 100vh;
+  text-align: center;
+  background-color: burlywood;
+}
+
+input {
+  position: relative;
+}
+</style>
